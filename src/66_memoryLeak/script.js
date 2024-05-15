c31a2dd9f23679abe70206bef7f0b3a0adcbb5d9
Vladimir <likholetov.vladimir2@yandex.ru>
2024-05-15 23:43:25 +0300

    add script.js in folder 66

---
+++ src/66_memoryLeak/script.js
@@ -0,0 +1,36 @@
+// function func() {
+//   window.smth = "string";
+// }
+
+// const someRes = getData();
+// const node = document.querySelector('.class');
+
+// setInterval(function() {
+//   if (node) {
+//     node.innerHTML = someRes;
+//   }
+// }, 1000);
+
+// function outer() {
+//   const potentiallyHugeArray = [];
+//   return function inner() {
+//     potentiallyHugeArray.push('Hello');
+//     console.log('Hello');
+//   };
+// }
+
+// const sayHello = outer();
+
+function createElement() {
+  const div = document.createElement('div');
+  div.id = 'test';
+  document.body.append(testDiv);
+}
+
+createElement();
+
+function deleteElement() {
+  document.body.removeChild[document.getElementById('test')];
+}
+
+deleteElement();
